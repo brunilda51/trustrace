edd0a9a634afcb2d7cffc3b8584bcb34acffea9c
Brunilda Metaj <b.metaj51@gmail.com>
2024-03-15 14:50:06 +0100

    Cors Changes

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,13 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // or specify the origins you want to allow
+    methods: "GET,PUT,POST,DELETE", // include PUT method
+    allowedHeaders: "Content-Type,Authorization", // include any custom headers your PUT request might include
+  })
+);
 
 // Parent Router
 const apiRouter = express.Router();
