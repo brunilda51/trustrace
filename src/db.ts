219be720a4709371a12a5e3fe5cfd0da082310ef
Brunilda Metaj <b.metaj51@gmail.com>
2024-03-15 15:36:34 +0100

    Fix

--- src/db.ts
+++ src/db.ts
@@ -5,14 +5,14 @@ export async function connectToDatabase() {
   try {
     let mongoHost;
 
-    if (process.env.NODE_ENV === "production") {
-      mongoHost = "localhost";
+    if (process.env.NODE_ENV !== "production") {
+      mongoHost = "localhost:27017";
     } else {
-      mongoHost = "13.38.34.81";
+      mongoHost = "13.38.34.81:27018";
     }
 
     // Construct the MongoDB connection string
-    const mongoURI = `mongodb://${mongoHost}:27018/trustmen`;
+    const mongoURI = `mongodb://${mongoHost}/trustmen`;
     await mongoose.connect(mongoURI);
     console.log("Connected to MongoDB");
   } catch (error) {
